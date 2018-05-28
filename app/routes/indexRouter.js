27ea7be9099ecca7634287c885c6282f78a0dfbb
Igor Silva <psilva.igor@gmail.com>
2018-05-28 17:27:10 -0300

    logando o erro

--- app/routes/indexRouter.js
+++ app/routes/indexRouter.js
@@ -1,6 +1,12 @@
 module.exports = function(application){
     
     application.get('/', function(req, res){
-        application.app.controllers.indexController.home(application, req, res);
+        console.log('Oi');
+        console.log(req);
+        try {
+            application.app.controllers.indexController.home(application, req, res);
+        } catch (error) {
+            console.log(error);
+        }
     });
 }
