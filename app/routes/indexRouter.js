650bc27957757d7f068224be35f6b9b5d3bf23b7
Igor Silva <psilva.igor@gmail.com>
2018-05-28 17:45:52 -0300

    Teste de consign 2

--- app/routes/indexRouter.js
+++ app/routes/indexRouter.js
@@ -3,7 +3,7 @@ module.exports = function(app){
     application.get('/', function(req, res){
         try {
             console.log(JSON.stringify(application));
-            app.app.controllers.indexController.home(application, req, res);
+            app.app.controllers.indexController.home(app, req, res);
         } catch (error) {
             console.log(error);
         }
