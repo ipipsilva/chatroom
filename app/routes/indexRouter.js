c51d46abada73a940731930bef6eeef7b23a3677
Igor Silva <psilva.igor@gmail.com>
2018-05-28 17:35:08 -0300

    Logando o application

--- app/routes/indexRouter.js
+++ app/routes/indexRouter.js
@@ -1,9 +1,8 @@
 module.exports = function(application){
     
     application.get('/', function(req, res){
-        console.log('Oi');
-        console.log(req);
         try {
+            console.log(JSON.stringify(application));
             application.app.controllers.indexController.home(application, req, res);
         } catch (error) {
             console.log(error);
